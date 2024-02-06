ef53700b07828ee551f425142b09f73e55f1fa50
Bhavik Narang <bhavik.narang99@gmail.com>
2024-02-06 22:01:55 +0000

    set up containers hiding and showing

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,37 @@
-// Example of DOM Manipulation with Pure JavaScript
-
-const changeSubtextBackground = () => {
-  // document.querySelector() selects an HTML element from the page by its attribute
-    // ids are selected with the '#' preceeding the ID name
-
-  // here we are selecting the <p id='subtext'></p> tag in our index.html
-  const subtext = document.querySelector('#subtext');
-  // this sets the 'style' attribute of the HTML element we selected
-  // the same as writing <p id='subtext' style="background-color: blue;"></p> in HTML
-  subtext.setAttribute('style', "background-color: blue" );
+function handleButtonClick() {
+  alert("Yay! You said yes! 😍");
 }
 
-// calling a function from other.js
-// remember to import it into index.html and in the correct order of what is initiated
-exampleJavaScriptFn();
+counter = 0
 
+document.addEventListener("DOMContentLoaded", function () {
+  const questionContainer = document.getElementById("question-container");
+  const warningContainer = document.getElementById("warning-container");
+  const happyContainer = document.getElementById("happy-container");
+  const noButton = document.getElementById("no-button");
 
-// now we call the function defined above
-// it will select the HTML element with id='subtext'
-// then it will color its background blue
-changeSubtextBackground();
+  noButton.addEventListener("mouseover", function (event) {
+    const top = getRandomNumber((questionContainer.offsetHeight - noButton.offsetHeight)*0.3);
+    const left = getRandomNumber((questionContainer.offsetWidth - noButton.offsetWidth)*0.3);
 
+    noButton.style.top = top+"px"
+    noButton.style.left = left+"px"
+
+    counter++
+
+    if (counter % 10 == 0){
+      $("#question-container").fadeOut('fast').delay(5000);
+      $("#warning-container").fadeIn('slow').animate({left: '250px'});;
+    }
+
+    console.log(counter)
+  });
+
+  const getRandomNumber = (num) => {
+    const ranNum = Math.random();
+    const posNeg = ranNum < 0.5 ? -ranNum : ranNum;
+    const val = Math.floor(posNeg * num);
+    return val;
+  };
+});
 
