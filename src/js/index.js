240a0222234693d583f0b20cb6fe7e30ceab0d23
Bhavik Narang <bhavik.narang99@gmail.com>
2024-02-06 22:27:21 +0000

    adding listeners for every option

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,24 @@
 function handleButtonClick() {
+  
   alert("Yay! You said yes! 😍");
 }
 
+const getRandomNumber = (num) => {
+    const ranNum = Math.random();
+    const posNeg = ranNum < 0.5 ? -ranNum : ranNum;
+    const val = Math.floor(posNeg * num);
+    return val;
+  };
+
 counter = 0
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const questionContainer = document.getElementById("question-container");
-  const warningContainer = document.getElementById("no-container");
-  const happyContainer = document.getElementById("happy-container");
+  const noContainer = document.getElementById("no-container");
+  const yesContainer = document.getElementById("happy-container");
   const noButton = document.getElementById("no-button");
+  const yesButton = document.getElementById("yes-button");
 
   noButton.addEventListener("mouseover", function (event) {
     const top = getRandomNumber((questionContainer.offsetHeight - noButton.offsetHeight)*0.3);
@@ -19,19 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     counter++
 
-    if (counter % 10 == 0){
-      $("#question-container").fadeOut('fast').delay(5000);
-      $("#no-container").fadeIn('slow').animate({left: '250px'});;
+    if (counter % 2 == 0){
+      $("#question-container").hide().delay(50000);
+      $("#no-container").fadeIn('slow');;
     }
 
     console.log(counter)
   });
 
-  const getRandomNumber = (num) => {
-    const ranNum = Math.random();
-    const posNeg = ranNum < 0.5 ? -ranNum : ranNum;
-    const val = Math.floor(posNeg * num);
-    return val;
-  };
-});
+  noButton.addEventListener("click", function (event) {
+    console.log("no clicked")
+  });
 
+  yesButton.addEventListener("click", function (event) {
+    console.log("yes clicked")
+  });
+  
+});
